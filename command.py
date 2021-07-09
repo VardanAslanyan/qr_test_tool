8e598f501acd25216392b79b91d2fc7dc04c2471
Vardan <var.aslanyan@future.am>
2021-07-09 23:17:54 +0400

    work in treading mode

--- command.py
+++ command.py
@@ -1,5 +1,6 @@
 import argparse
 from modules import final
+import threading
 
 
 parser = argparse.ArgumentParser(prog='QR Test', usage='command.py [-h] sub',
@@ -9,8 +10,13 @@ parser.add_argument('-t', '--test', type=int)
 args = parser.parse_args()
 
 if __name__ == '__main__':
+    first_part = ("01924939", "19032625", "01362152", "14828203", "03284023", "90173073")
     direct = "http://192.168.7.112:6090"
     proxy = "http://192.168.7.145:6010"
-    final(proxy, stop_func=args.test)
+    first = threading.Thread(target=final, args=(first_part, proxy,), kwargs={'stop_func': args.test})
+    second_part = ('90301498', '90301501', '90301530', '90301540', '90301543', '90301614')
+    second = threading.Thread(target=final, args=(second_part, proxy,), kwargs={'stop_func': args.test})
+    first.start()
+    second.start()
 
 
--- modules/payment_system.py
+++ modules/payment_system.py
@@ -2,8 +2,9 @@ import datetime
 import random
 
 
-def answer(rrn, mid, tid, amount, transaction_id, bankname='FPS'):
-    status = random.choices(['0', '5', '100', '103', '109', '116', '120', '904', '912', '913', '914', '923'])[0]
+def answer(rrn, mid, tid, amount, transaction_id):
+    bankname = random.choice(['FPS', 'Evocabank', 'MobiDram', 'ConverseBank'])
+    status = random.choice(['0', '5', '100', '103', '109', '116', '120', '904', '912', '913', '914', '923'])
     body = {'version': '1.0',
             'method': 'status',
             'id': str(transaction_id),
